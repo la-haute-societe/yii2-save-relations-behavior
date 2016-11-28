49cae2c76bfe3c112b275b3c0f25b01a27e489f1
Alban Jubert <alban@lahautesociete.com>
2016-11-28 09:10:08 +0100

    Fix for issue #12

---
+++ tests/models/DummyModel.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @link http://www.lahautesociete.com
+ * @copyright Copyright (c) 2016 La Haute Société
+ */
+
+namespace tests\models;
+
+use lhs\Yii2SaveRelationsBehavior\SaveRelationsBehavior;
+
+/**
+ * DummyModel class
+ *
+ * @author albanjubert
+ **/
+class DummyModel extends \yii\db\ActiveRecord
+{
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'dummy';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'saveRelations' => [
+                'class'     => SaveRelationsBehavior::className(),
+                'relations' => ['children']
+            ],
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getChildren()
+    {
+        return $this->hasOne(DummyModel::className(), ['id' => 'parent_id']);
+    }
+
+}
