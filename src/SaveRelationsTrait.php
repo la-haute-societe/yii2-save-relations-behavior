297b3c15060cfda884facceaa772299f344fcd5e
Papp Péter <papppeter@gmail.com>
2019-09-13 10:42:00 +0200

    work with unclead multi input
    improvement.

--- src/SaveRelationsTrait.php
+++ src/SaveRelationsTrait.php
@@ -2,6 +2,12 @@
 
 namespace lhs\Yii2SaveRelationsBehavior;
 
+/**
+ * Trait SaveRelationsTrait
+ * @package lhs\Yii2SaveRelationsBehavior
+ *
+ * @mixin SaveRelationsBehavior
+ */
 trait SaveRelationsTrait
 {
 
@@ -9,8 +15,44 @@ trait SaveRelationsTrait
     {
         $loaded = parent::load($data, $formName);
         if ($loaded && $this->hasMethod('loadRelations')) {
+            $this->_prepareLoadData($data);
+
             $this->loadRelations($data);
         }
         return $loaded;
+    }
+
+    /**
+     * @param $data
+     */
+    private function _prepareLoadData(&$data) {
+        $scope = $formName === null ? $this->formName() : $formName;
+
+        foreach ($this->relations as $key => $value) {
+            if (is_int($key)) {
+                $relationName = $value;
+            } else {
+                $relationName = $key;
+            }
+
+            if(!isset($data[$scope][$relationName])) {
+                continue;
+            }
+
+            $relation = $this->getRelation($relationName);
+
+            if(!$relation) {
+                continue;
+            }
+
+            $modelClass = $relation->modelClass;
+            /** @var ActiveQuery $relationalModel */
+            $relationalModel = new $modelClass;
+            $keyName = $relationalModel->formName();
+
+            $data[$keyName] = $data[$scope][$relationName];
+
+            unset($data[$scope][$relationName]);
+        }
     }
 }
